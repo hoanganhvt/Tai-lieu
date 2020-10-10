1a94bb0dca2e6e57ac6fcf03c3e5c75410558a58
hoanganhvt <60812137+hoanganhvt@users.noreply.github.com>
2020-10-10 01:39:24 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int sum(int N){
+	int cnt  = 0;
+	while(N > 0){
+		cnt += N%10;
+		N = N/10;
+	}
+	return cnt;
+}
+
+
+int lengthOfNumber(int N){
+	int cnt  = 0;
+	while(N > 0){
+		cnt += 1;
+		N = N/10;
+	}
+	return cnt;
+}
+int bai3(int N){
+	int cnt = 0;
+	for(int i = 9*lengthOfNumber(N);i >= 1 ;i--){
+		if (sum(N-i-sum(i))==i){
+			cout<<(N-i-sum(i))<<endl;
+			cnt++;
+		} 
+	}
+	return cnt;
+}
+int main(){
+
+	cout << bai3(9939) << endl;
+}
